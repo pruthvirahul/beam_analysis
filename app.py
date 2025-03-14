0461d1bdb90b6d7df466efa620a6122170a5fda3
pruthvirahul <94065613+pruthvirahul@users.noreply.github.com>
2025-03-14 23:55:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -107,9 +107,5 @@ def download():
 
     return send_file(pdf_file, as_attachment=True)
 
-if __name__ == '__main__':
-    if not os.path.exists("static"):
-        os.mkdir("static")
-    app.run(debug=True)
-
-
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
